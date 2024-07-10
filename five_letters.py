8ecddcbcb2206e93a8a6d61064856cb00eb239c0
Eric Grimaldi <EricAndrewGrimaldi@Gmail.com>
2024-07-10 13:48:56 -0400

    added command line args
    also beautified console output a lil bit

--- five_letters.py
+++ five_letters.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import logging
 import math
@@ -5,17 +6,60 @@ import re
 from collections import Counter
 from copy import deepcopy
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
 WORD_LENGTH = 5
 MAX_GUESSES = 6
-WS_PRINT_MAX = 100
+MAX_PRINT_TH = 100
+GREEN = '\u001b[32m'
+YELLOW = '\u001b[33m'
+RESET = '\u001b[0m'
+
+def get_args() -> Dict:
+    parser = argparse.ArgumentParser('analyze the quality of Wordle guesses')
+    parser.add_argument('-pm', '--post-mortem',
+        action='store_true',
+        help='switch to post mortem mode',
+        dest='post-mortem'
+    )
+    parser.add_argument('-g', '--guesses',
+        required=False,
+        help=f'comma separated list of guesses',
+        dest='guesses'
+    )
+    parser.add_argument('-a', '--answer',
+        required=False,
+        help='true answer',
+        dest='answer'
+    )
+    parser.add_argument('-th', '--print-threshold',
+        default=0,
+        type=int,
+        required=False,
+        help='print possible answers if fewer than this number remain',
+        dest='print-threshold'
+    )
+    args = vars(parser.parse_args())
+    if args['post-mortem']:
+        if 'answer' not in args:
+            args['answer'] = input('enter the true answer\n')
+        assert_valid_guess(args['answer'])
+        if 'guesses' not in args:
+            args['guesses'] = input(f'enter a comma separated list of guesses\n')
+        args['guesses'] = args['guesses'].split(',')
+        assert len(args['guesses']) <= MAX_GUESSES, f'invalid number of guesses: {len(args['guesses'])}. number of guesses must be at most {MAX_GUESSES}.'
+        for guess in args['guesses']:
+            assert_valid_guess(guess)
+    return args
+
+def assert_valid_guess(guess) -> None:
+    assert re.match('^[a-z]{5}$', guess), f'invalid guess {guess}. valid guesses are strings of exactly {WORD_LENGTH} lowercase letters of the english alphabet.'
 
 class FiveLetters:
     def __init__(self) -> None:
         with open(Path(__file__).parent / 'five_letters_common.json', 'r') as data_file:
             self.data = json.load(data_file)
-            logging.warning(f'loaded {len(self.data)} five letter words from Datamuse. note: Datamuse contains many names, acronyms, etc that do not qualify as normal words.')
+            logging.warning(f'loaded {len(self.data)} five letter words from Datamuse.\n\tnote: Datamuse contains many names, acronyms, etc that do not qualify as normal words.')
             self.reset_workspace()
         self.final_messages = {
             -1: 'oof...',
@@ -26,18 +70,9 @@ class FiveLetters:
             5: 'not your best work.',
             6: 'close call.',
         }
-        self.error_messages = {
-            'invalid guess': f'invalid guess. valid guesses are strings of exactly {WORD_LENGTH} lowercase letters of the english alphabet.',
-            'invalid color string': f'invalid color string. valid color strings are strings of exactly {WORD_LENGTH} lowercase letters from the set {"g", "y", "b"}.',
-        }
     def reset_workspace(self) -> None:
         self.workspace = deepcopy(self.data)
-    def valid_guess(self, guess: str) -> bool:
-        return True if re.match('^[a-z]{5}$', guess) else False
-    def valid_color_string(self, color_string: str) -> bool:
-        return True if re.match('^[gyb]{5}$', color_string) else False
     def guess_2_color_string(self, guess: str, answer: str) -> str:
-        assert self.valid_guess(guess), self.error_messages['invalid guess']
         color_string = ''
         guess_count_dict = Counter()
         answer_count_dict = Counter(answer)
@@ -50,9 +85,7 @@ class FiveLetters:
             else:
                 color_string += 'b'
         return color_string
-    def guess_2_pattern(self, guess: str, color_string: str = 'bbbbb') -> str:
-        assert self.valid_guess(guess), self.error_messages['invalid guess']
-        assert self.valid_color_string(color_string), self.error_messages['invalid color string']
+    def guess_2_pattern(self, guess: str, color_string: str) -> str:
         relevant_count_dict = Counter()
         for i in range(WORD_LENGTH):
             letter, color = guess[i], color_string[i]
@@ -99,57 +132,76 @@ class FiveLetters:
                     else:
                         logging.warning('trips? impossible...')
         return '|'.join(clauses)
-    def apply_guess(self, guess: str, color_string: str = 'bbbbb') -> float:
-        assert self.valid_guess(guess), self.error_messages['invalid guess']
-        assert self.valid_color_string(color_string), self.error_messages['invalid color string']
+    def apply_guess(self, guess: str, color_string: str) -> float:
         len_old_workspace = len(self.workspace)
         pattern = self.guess_2_pattern(guess, color_string)
         self.workspace = [element for element in self.workspace if not re.match(pattern, element['word'])]
         info_gained = math.log(float(len_old_workspace/float(len(self.workspace))), 2)
         return info_gained
-    def post_mortem(self, guesses: List[str], answer: str, ws_print_condition: int = 0) -> None:
+    def apply_color(self, guess: str, color_string: str) -> str:
+        guess_with_color = ''
+        for i in range(WORD_LENGTH):
+            match color_string[i]:
+                case 'g':
+                    temp = GREEN + guess[i] + RESET
+                case 'y':
+                    temp = YELLOW + guess[i] + RESET
+                case _:
+                    temp = guess[i]
+            guess_with_color += temp
+        return guess_with_color
+    def post_mortem(self, guesses: List[str], answer: str, print_threshold: int = 0) -> None:
         # TODO reduce duplicated code
-        if ws_print_condition > WS_PRINT_MAX:
-            logging.warning(f'you probably don\'t want to print {ws_print_condition} words in a python for loop\nclamping ws_print_condition to {WS_PRINT_MAX}.')
+        if print_threshold > MAX_PRINT_TH:
+            logging.warning(f'you probably don\'t want to print {print_threshold} words in a python for loop\n\tclamping print_threshold to {MAX_PRINT_TH}.')
         self.reset_workspace()
         total_info_gained = 0
-        for guess in guesses:
+        for guess_number, guess in enumerate(guesses):
+            assert_valid_guess(guess)
             color_string = self.guess_2_color_string(guess, answer)
             info_gained = self.apply_guess(guess, color_string)
             total_info_gained += info_gained
-            print(f'guessed {guess}, giving {color_string}')
+            guess_with_color = self.apply_color(guess, color_string)
+            print(f'guess {guess_number}: {guess_with_color}')
             print(f'\t{len(self.workspace)} words remaining')
-            if 1 < len(self.workspace) <= ws_print_condition:
+            if 1 < len(self.workspace) <= print_threshold:
                 for element in self.workspace:
                     print(f'\t\t{element["word"]}')
             print(f'\t{info_gained} bits of information gained')
             print(f'\t{total_info_gained} total bits of information gained')
         final_message = f'solved in {len(guesses)} - {self.final_messages[len(guesses)]}' if guesses[-1] == answer else self.final_messages[-1]
         print(final_message)
-    def live_analysis(self, ws_print_condition: int = 0) -> None:
+    def live_analysis(self, print_threshold: int = 0) -> None:
         # TODO reduce duplicated code
-        if ws_print_condition > WS_PRINT_MAX:
-            logging.warning(f'you probably don\'t want to print {ws_print_condition} words in a python for loop\nclamping ws_print_condition to {WS_PRINT_MAX}.')
+        if print_threshold > MAX_PRINT_TH:
+            logging.warning(f'you probably don\'t want to print {print_threshold} words in a python for loop\n\tclamping print_threshold to {MAX_PRINT_TH}.')
         self.reset_workspace()
-        num_guesses = 0
+        guess_number = 0
         solved = False
         total_info_gained = 0
-        while num_guesses < MAX_GUESSES and not solved:
+        while guess_number < MAX_GUESSES and not solved:
             guess, color_string = input('enter a guess and color string, separated by white space\n').split()
+            assert_valid_guess(guess)
+            assert re.match('^[gyb]{5}$', color_string), f'invalid color string: "{color_string}". valid color strings are strings of exactly {WORD_LENGTH} lowercase letters from the set {"g", "y", "b"}.'
             info_gained = self.apply_guess(guess, color_string)
             total_info_gained += info_gained
-            print(f'guessed {guess}, giving {color_string}')
+            guess_with_color = self.apply_color(guess, color_string)
+            print(f'guess {guess_number}: {guess_with_color}')
             print(f'\t{len(self.workspace)} words remaining')
-            if 1 < len(self.workspace) <= ws_print_condition:
+            if 1 < len(self.workspace) <= print_threshold:
                 for element in self.workspace:
                     print(f'\t\t{element["word"]}')
             print(f'\t{info_gained} bits of information gained')
             print(f'\t{total_info_gained} total bits of information gained')
-            num_guesses += 1
+            guess_number += 1
             solved = True if color_string == 'ggggg' else False
-        final_message = f'solved in {num_guesses} - {self.final_messages[num_guesses]}' if solved else self.final_messages[-1]
+        final_message = f'solved in {guess_number} - {self.final_messages[guess_number]}' if solved else self.final_messages[-1]
         print(final_message)
 
 if __name__ == '__main__':
+    args = get_args()
     fl = FiveLetters()
-    fl.live_analysis(ws_print_condition=50)
+    if args['post-mortem']:
+        fl.post_mortem(guesses=args['guesses'], answer=args['answer'], print_threshold=args['print-threshold'])
+    else:
+        fl.live_analysis(print_threshold=args['print-threshold'])
